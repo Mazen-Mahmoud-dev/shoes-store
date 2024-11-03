42f2235a42bab7ba8489c33b676cc9059d12ca16
Mazen Mahmoud <bra74039@gmail.com>
2024-11-03 18:56:13 +0200

    change server url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'https://shoes-store-server-q7q2.onrender.com'
 // export const BASE_URL = 'https://shoes-store-server-1.onrender.com'
-export const BASE_URL = 'http://localhost:5000'
+export const BASE_URL = 'https://shoes-store-server-1.onrender.com'
